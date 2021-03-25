bbd41a147652965008498cf43f90ad9dcfd52432
Pierre VILLIERS <pierre.villiers@ynov.com>
2021-03-25 10:25:07 +0100

    Ajout du Store redux + action profile + reducer profile

---
+++ src/actions/profile.js
@@ -0,0 +1,11 @@
+export const UPDATE_PROFILE = 'UPDATE_PROFILE'
+export const RESET_PROFILE = 'RESET_PROFILE'
+
+export const updateProfile = newProfile => ({
+  type: UPDATE_PROFILE,
+  payload: newProfile
+})
+
+export const updateProfile = newProfile => ({
+  type: RESET_PROFILE
+})
