5e9444a13c54b93adcd56f5b5444be8831d2a58e
YlaneT <yloo318@yahoo.fr>
2021-03-25 10:12:52 +0100

    Router

---
+++ src/config/router.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
+
+import Home from '../screens/Home'
+import Profile from '../screens/Profile'
+import SpaceTravel from '../screens/SpaceTravel'
+import GameOver from '../screens/GameOver'
+import Leaderboards from '../screens/Leaderboards'
+
+const router = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/profile' component={Profile} />
+        <Route exact path='/spacetravel' component={SpaceTravel} />
+        <Route exact path='/gameover' component={GameOver} />
+        <Route exact path='/leaderboards' component={Leaderboards} />
+      </Switch>
+    </Router>
+  )
+}
+
+export default router
---
+++ src/screens/GameOver.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// Ecran de fin de partie (victoire ou défaite)
+const GameOver = () => {
+  return <div></div>
+}
+
+export default GameOver
