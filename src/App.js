7fdece475db1adfcab4122eb82ddb9c3e2f8105b
JR-2897 <34342750+JR-2897@users.noreply.github.com>
2021-04-01 11:53:42 +0200

    JRG - Correction bug Firebase (#15)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './configs/store'
 import Routes from './configs/router'
+import { getToken } from './configs/firebase'
 
 import './configs/translations'
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    getToken()
+  }, [])
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
